6a2f0e9e7d110d344727e03e44dee1422dd23387
keepwn <keepwn@gmail.com>
2021-07-15 13:07:26 +0800

    fix: support set PATH environment
    Sometimes extension couldn't get env from osquery.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 	"time"
 
 	"github.com/osquery/osquery-go"
@@ -15,20 +16,20 @@ import (
 
 var (
 	c *cache.Cache
+
+	flSocket = flag.String("socket", "", "Path to the UNIX domain socket of extension")
+	flTimeout = flag.Int("timeout", 5, "Wait for autoload extension")
+	flInterval = flag.Int("interval", 5, "Delay of connecting check")
+	flPathEnv = flag.String("pathenv", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin", "Set to PATH environment")
+	flVerbose = flag.Bool("verbose", false, "")
 )
 
 func main() {
-	flSocket := flag.String("socket", "", "")
-	flTimeout := flag.Int("timeout", 0, "")
-	flag.Int("interval", 0, "")
-	flag.Bool("verbose", false, "")
 	flag.Parse()
 
 	if *flSocket == "" {
 		log.Fatalln("--socket flag cannot be empty")
 	}
-
-	timeout := time.Duration(*flTimeout) * time.Second
 
 	// allow for osqueryd to create the socket path
 	time.Sleep(2 * time.Second)
@@ -36,11 +37,15 @@ func main() {
 	server, err := osquery.NewExtensionManagerServer(
 		"com.keepwn.extension.inspecquery",
 		*flSocket,
-		osquery.ServerTimeout(timeout),
+		osquery.ServerTimeout(time.Duration(*flTimeout) * time.Second),
+		osquery.ServerPingInterval(time.Duration(*flInterval) * time.Second),
 	)
 	if err != nil {
 		log.Fatalf("Error creating extension: %s\n", err)
 	}
+
+	// set PATH env
+	_ = os.Setenv("PATH", *flPathEnv)
 
 	// init cache to improve performance
 	c = cache.New(1*time.Minute, 10*time.Minute)
